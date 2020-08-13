0a74f76d9a7cb0150c6f23ddeeb3e7a9f198976a
Talal <TalalOthman2000@gmail.com>
2020-08-14 04:47:15 +0800

    added most of the elements in the desgin

--- covid-19-tracker/src/App.js
+++ covid-19-tracker/src/App.js
@@ -1,8 +1,10 @@
 import React, {useState, useEffect} from 'react';
 import FormControl from '@material-ui/core/FormControl';
 import './App.css';
-import { Select } from '@material-ui/core';
+import { Select, Card, CardContent } from '@material-ui/core';
 import { MenuItem } from '@material-ui/core';
+import InfoBox from './InfoBox';
+import Map from './Map';
 
 function App() {
   const [countries, setCountries] = useState([]);
@@ -29,12 +31,15 @@ function App() {
 
   const onCountryChange = async (event) =>{
     const countryCode = event.target.value;
+    setCountry(countryCode);
     
   }
 
   return (
     <div className="app">
-      <div className= "app__header">
+
+      <div className="app__left">
+       <div className= "app__header">
       <h1>Covid 19 Tracker</h1>
       <FormControl className="app__dropdown">
         <Select
@@ -48,10 +53,28 @@ function App() {
               <MenuItem value={country.value}>{country.name}</MenuItem>
             ))
           }
-
         </Select>
       </FormControl>
       </div>
+
+
+      <div className="app__stats">
+        <InfoBox title="Coronavirus Cases" cases="123" total="2000"/>
+        <InfoBox title="Coronavirus Cases" cases="1234" total="12"/>
+        <InfoBox title="Coronavirus Cases" cases= "69" total= "69"/>
+      </div>
+
+      <Map />
+      </div>
+      
+
+
+      <Card className="app__right">
+      <CardContent> 
+        <h3>Live Cases by Country</h3>
+        <h3>Worldwide new cases</h3>
+      </CardContent>
+      </Card>
       
 
 
