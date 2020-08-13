89dc0576d710b5a4618c55e5e58f693ce337fcdc
Talal <TalalOthman2000@gmail.com>
2020-08-13 08:20:37 +0800

    first commit

---
+++ covid-19-tracker/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Covid 19 Tracker</h1>
+      <FormControl className = "app__dropdown">
+        
+      </FormControl>
+      {/*Header*/} 
+
+      
+    </div>
+  );
+}
+
+export default App;
