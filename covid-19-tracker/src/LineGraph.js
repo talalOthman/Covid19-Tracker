ac6e0ef2e06dded65b53c396a608bcf1d4b44ca6
Talal <TalalOthman2000@gmail.com>
2020-08-16 06:56:18 +0800

    code refactoring

--- covid-19-tracker/src/LineGraph.js
+++ covid-19-tracker/src/LineGraph.js
@@ -48,7 +48,7 @@ const options = {
     },
 }
 
-function LineGraph() {
+function LineGraph({casesType = "cases"}) {
     const [data, setData] = useState({});
 
     const buildChartData = (data, casesType = "cases") =>{
@@ -68,13 +68,19 @@ function LineGraph() {
     };
 
     useEffect(() => {
-        fetch('https://disease.sh/v3/covid-19/historical/all?lastdays=120')
-        .then(response => response.json())
-        .then(data => {
-            const chartData = buildChartData(data);
-            setData(chartData);
+        const fetchData = async () => {
+        await fetch('https://disease.sh/v3/covid-19/historical/all?lastdays=120')
+        .then((response) => {
+            return response.json();
         })
-    }, [])
+        .then(data => {
+            const chartData = buildChartData(data, casesType);
+            setData(chartData);
+        });
+        };
+        fetchData();
+        
+    }, [casesType])
 
 
 
