f2d0063a191cd63c18b81bba82806866c0efd1b7
Talal <TalalOthman2000@gmail.com>
2020-08-18 01:08:29 +0800

    added top borders whenever the user click a infoBox

--- covid-19-tracker/src/App.js
+++ covid-19-tracker/src/App.js
@@ -106,9 +106,9 @@ function App() {
 
 
         <div className="app__stats">
-          <InfoBox onClick={(e) => setCasesType('cases')}title="Coronavirus Cases" cases={prettyPrintStat(countryInfo.todayCases)} total={prettyPrintStat(countryInfo.cases)} />
-          <InfoBox onClick={(e) => setCasesType('recovered')}title="Recovered" cases={prettyPrintStat(countryInfo.todayRecovered)} total={prettyPrintStat(countryInfo.recovered)} />
-          <InfoBox onClick={(e) => setCasesType('deaths')}title="Deaths" cases={prettyPrintStat(countryInfo.todayDeaths)} total={prettyPrintStat(countryInfo.deaths)} />
+          <InfoBox isRed active={casesType === "cases"} onClick={(e) => setCasesType('cases')} title="Coronavirus Cases" cases={prettyPrintStat(countryInfo.todayCases)} total={prettyPrintStat(countryInfo.cases)} />
+          <InfoBox active={casesType === "recovered"} onClick={(e) => setCasesType('recovered')} title="Recovered" cases={prettyPrintStat(countryInfo.todayRecovered)} total={prettyPrintStat(countryInfo.recovered)} />
+          <InfoBox isRed active={casesType === "deaths"} onClick={(e) => setCasesType('deaths')} title="Deaths" cases={prettyPrintStat(countryInfo.todayDeaths)} total={prettyPrintStat(countryInfo.deaths)} />
         </div>
 
         <Map
@@ -125,8 +125,8 @@ function App() {
         <CardContent>
           <h3>Live Cases by Country</h3>
           <Table countries={tableData} />
-          <h3>Worldwide new cases</h3>
-          <LineGraph />
+          <h3>Worldwide new {casesType}</h3>
+          <LineGraph casesType={casesType}/>
         </CardContent>
       </Card>
 
--- covid-19-tracker/src/InfoBox.js
+++ covid-19-tracker/src/InfoBox.js
@@ -2,10 +2,10 @@ import React from 'react'
 import {Card, CardContent, Typography} from '@material-ui/core';
 import './InfoBox.css';
 
-function InfoBox({title, cases, total, ...props}) {
+function InfoBox({title, cases, isRed, active, total, ...props}) {
     return (
         <Card 
-        className= "infoBox"
+        className= {`infoBox ${active && "infoBox--selected"} ${isRed && "infoBox--red"}`}
         onClick={props.onClick}
         >
             <CardContent>
@@ -14,7 +14,7 @@ function InfoBox({title, cases, total, ...props}) {
                     {title}
                 </Typography>
                 
-                <h2 className="infoBox__cases">{cases}</h2>
+                <h2 className={`infoBox__cases ${!isRed && "infoBox__cases--green"}` }>{cases}</h2>
                 
                 <Typography className="infoBox__total" color="textSecondary">
                     {total} Total
